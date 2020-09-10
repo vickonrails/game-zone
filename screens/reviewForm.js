db0ede7be214f9c4ac930319d2aeae79306675d4
vickonrails <victorofoegbu0009@gmail.com>
2020-09-10 19:26:31 +0100

    feat: implement form validation with yup

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -1,6 +1,18 @@
 import React from "react";
-import { Alert, Button, TextInput, View, Text } from "react-native";
+import { Button, TextInput, View, Text } from "react-native";
 import { Formik } from "formik";
+import * as yup from "yup";
+
+const reviewSchema = yup.object({
+  title: yup.string().required().min(4),
+  body: yup.string().required().min(8),
+  rating: yup
+    .string()
+    .required()
+    .test("is-num-1-5", "Rating must be a number from 1 - 5", (val) => {
+      return parseInt(val) < 6 && parseInt(val) > 0;
+    }),
+});
 
 import { globalStyles } from "../styles/global";
 
@@ -8,19 +20,33 @@ export default function ReviewForm({ addReview }) {
   return (
     <View style={globalStyles.container}>
       <Formik
-        initialValues={{ body: "" }}
-        onSubmit={(values) => {
+        validationSchema={reviewSchema}
+        initialValues={{ body: "", title: "", rating: "" }}
+        onSubmit={(values, actions) => {
           addReview(values);
+          actions.resetForm();
         }}
       >
-        {({ handleSubmit, handleChange, values }) => (
+        {({
+          handleSubmit,
+          handleBlur,
+          handleChange,
+          values,
+          touched,
+          errors,
+        }) => (
           <View>
             <TextInput
               style={globalStyles.input}
               placeholder="Review title"
               onChangeText={handleChange("title")}
               value={values.title}
+              onBlur={handleBlur("title")}
             />
+
+            <Text style={globalStyles.errorText}>
+              {touched.title && errors.title}
+            </Text>
 
             <TextInput
               multiline
@@ -28,7 +54,11 @@ export default function ReviewForm({ addReview }) {
               placeholder="Review body"
               onChangeText={handleChange("body")}
               value={values.body}
+              onBlur={handleBlur("body")}
             />
+            <Text style={globalStyles.errorText}>
+              {touched.body && errors.body}
+            </Text>
 
             <TextInput
               multiline
@@ -37,7 +67,11 @@ export default function ReviewForm({ addReview }) {
               onChangeText={handleChange("rating")}
               value={values.rating}
               keyboardType="numeric"
+              onBlur={handleBlur("rating")}
             />
+            <Text style={globalStyles.errorText}>
+              {touched.rating && errors.rating}
+            </Text>
 
             <Button title="Submit" onPress={handleSubmit} color="maroon" />
           </View>
